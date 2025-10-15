1ede645552dd78d3524c71b6d34ad4faa16944fd
Alan <alanivan70093@gmail.com>
2025-10-15 14:58:10 -0600

    a few more changes....help me

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -32,18 +32,17 @@ export default function ProductCard({ name, price, label, image }: ProductCardPr
 
       {/* Contenedor de texto centrado verticalmente */}
       <div className="h-36 flex flex-col items-center justify-between text-center px-4">
-        {/* Nombre del producto */}
-        <h3 className="font-semibold text-base mb-1">{name}</h3>
+        {/* Nombre del producto - bajado con mt-4 */}
+        <h3 className="font-semibold text-base mb-1 mt-4">{name}</h3>
 
-        {/* Precio */}
+        {/* Precio - subido reduciendo el mt */}
         <p className="text-base font-bold text-fifaPurple mt-1">{price}</p>
       </div>
 
-{/* Botón */}
-<button className="bg-purple-700 text-white rounded-full px-4 py-2 text-sm transform transition-transform duration-200 hover:scale-105">
-  Agregar al Carro
-</button>
-
+      {/* Botón */}
+      <button className="bg-purple-700 text-white rounded-full px-4 py-2 text-sm transform transition-transform duration-200 hover:scale-105">
+        Agregar al Carro
+      </button>
     </div>
   );
 }
